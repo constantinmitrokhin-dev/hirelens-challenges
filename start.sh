f2cf2014345b758f1408b2b05f57dfb4f7c56141
constantinmitrokhin-dev <constantin.mitrokhin@gmail.com>
2025-09-02 17:48:34 -0300

    Update start.sh
    Se elimina el comando de migración de DB.

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd backend && npm install && npx sequelize-cli db:migrate &
+cd backend && npm install &
 cd frontend && npm install &
 wait
 npm start --prefix backend & npm start --prefix frontend
